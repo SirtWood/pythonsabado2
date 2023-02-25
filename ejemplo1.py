0ebd205d5073fa46a3bd1d36c48ea7da24f65bf2
Steven Torres Calero <stclink@gmail.com>
2023-02-25 13:07:44 -0500

    NT3 Se crea ejemplo con condicionales

---
+++ ejemplo1.py
@@ -0,0 +1,12 @@
+#Represa Hidroituango
+
+#ENTRADAS
+nivelAgua=float(input("Digite el nivel de agua: "))
+
+#PROCESO
+if nivelAgua >= 0 and nivelAgua <= 250:
+    print(f"El sensor marca {nivelAgua}, muy bajo.")
+elif nivelAgua > 250 and nivelAgua <= 400:
+    print(f"El sensor marca {nivelAgua}, operando normal.")
+else: 
+    print(f"El sensor marca {nivelAgua}, PELIGRO.")
